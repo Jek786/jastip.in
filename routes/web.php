e99359197a0fdbd8158862225fa89eddb6eaec45
iel <herobrinealegend@gmail.com>
2025-11-11 15:14:07 +0700

    fix: profile to bahasa

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
 });
 Route::get('/bahasa', function () {
     return view('bahasa'); 
-});
+})->name('bahasa');
 Route::get('/login', function () {
     return view('login');
 })->name('login');
