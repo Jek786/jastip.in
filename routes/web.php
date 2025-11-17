10c2f919f36e17562e23190736bbac7ad5abad35
nblashintaa <nabilashintaluthfia@gmail.com>
2025-11-17 13:06:52 +0700

    update web home page

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('home');
+})->name('home');
+Route::get('/', function () {
     return view('login');
 });
 Route::get('/bahasa', function () {
