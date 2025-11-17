eb5a17f8a1a7c226a3fce360da9902616607840e
nblashintaa <nabilashintaluthfia@gmail.com>
2025-11-17 16:43:26 +0700

    Add login and register flow with controller integration and route updates

--- routes/web.php
+++ routes/web.php
@@ -1,36 +1,27 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('home');
 })->name('home');
-Route::get('/', function () {
-    return view('login');
-});
+
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware('auth')->name('dashboard');
+
 Route::get('/bahasa', function () {
-    return view('bahasa'); 
+    return view('bahasa');
 })->name('bahasa');
-Route::get('/login', function () {
-    return view('login');
-})->name('login');
-Route::get('/register', function () {
-    return view('register');
-})->name('register');
+
+Route::get('/profile', function () {
+    return view('profile');
+})->middleware('auth')->name('profile');
+
 Route::get('/welcome', function () {
     return view('welcome');
 })->name('welcome');
-Route::get('/profile', function () {
-    return view('profile');
-})->name('profile');
