632a71242b65e08c5325f1e79eb99c0f409c05df
nblashintaa <nabilashintaluthfia@gmail.com>
2025-11-17 11:35:06 +0700

    Remove nested git repo jastip.in

---
+++ jastip.in/resources/views/register.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Register</title>
+</head>
+<body>
+    <h2>Halaman Register (sementara)</h2>
+    <a href="{{ route('welcome') }}">Kembali ke Welcome</a>
+</body>
+</html>
